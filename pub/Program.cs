1ab9f3d2a2814699c7624c15f9c568a5d832857b
natanaeladit <nata.adit@gmail.com>
2021-05-13 20:01:21 +0800

    pubsub

---
+++ pub/Program.cs
@@ -0,0 +1,36 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace pub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var publisher = new PublisherSocket())
+            {
+                publisher.Bind("tcp://*:5556");
+
+                int i = 0;
+
+                while (true)
+                {
+                    publisher
+                        .SendMoreFrame("A") // Topic
+                        .SendFrame(i.ToString()); // Message
+
+                    Console.WriteLine($"Send {i.ToString()}");
+
+                    i++;
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
---
+++ sub/Program.cs
@@ -0,0 +1,29 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var subscriber = new SubscriberSocket())
+            {
+                subscriber.Connect("tcp://127.0.0.1:5556");
+                subscriber.Subscribe("A");
+
+                while (true)
+                {
+                    var topic = subscriber.ReceiveFrameString();
+                    var msg = subscriber.ReceiveFrameString();
+                    Console.WriteLine("From Publisher: {0} {1}", topic, msg);
+                }
+            }
+        }
+    }
+}
